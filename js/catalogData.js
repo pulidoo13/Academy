6e137b22e35aa4806e7d31cdaac9e7e4cd05e607
Alfredo Pulido <apulidoo13@hotmail.com>
2017-08-10 15:52:42 -0500

    CatalogData.js Added

---
+++ js/catalogData.js
@@ -0,0 +1,42 @@
+var myCatalogData = {
+    "MyCollection": {
+        "Product1": {
+            "id": 0,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+
+        "Product2": {
+            "id": 1,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+        "Product3": {
+            "id": 2,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+        "Product4": {
+            "id": 3,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+        "Product5": {
+            "id": 4,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+        "Product6": {
+            "id": 5,
+            "title": "myProduct",
+            "description": "myDescription",
+            "imgUrl": "img/play_android_cell_phone_mobile_video_icon-icons.com_55211.png"
+        },
+
+    }
+}
